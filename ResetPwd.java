52ea6d5879875eeffbfc08f0774bda71a9276357
lizijian <lizijian@sdpjw.com>
2026-03-18 17:41:01 +0800

    feat: 删除

--- ResetPwd.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class ResetPwd {
-  public static void main(String[] args) {
-    System.out.print(new BCryptPasswordEncoder().encode(args[0]));
-  }
-}
